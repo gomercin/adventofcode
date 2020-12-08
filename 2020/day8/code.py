e0b94a72bdde8f44a0913569d975ead68dff4f35
Omer Karaduman <omer.karaduman@tomtom.com>
2020-12-08 10:54:26 +0100

    day 8; make the machine reusable and code cleanup

--- 2020/day8/code.py
+++ 2020/day8/code.py
@@ -4,19 +4,13 @@ input = []
 with open(os.path.join(sys.path[0], 'input.txt'), 'r') as in_file:
     input = in_file.readlines()
 
-acc = 0
-pos = 0
-
-visited = set()
-
-changed_ins = -1
-
-ins_set = []
-def convert():
-    for line in input:
-        line = line.strip()
-        parts = line.split(" ")
-        ins = parts[0]
+class Instruction:
+    JMP = "jmp"
+    NOP = "nop"
+    ACC = "acc"
+    def __init__(self, inp):
+        parts = inp.strip().split(" ")
+        self.opcode = parts[0]
         val = parts[1]
 
         mul = 1
@@ -24,57 +18,79 @@ def convert():
             mul = -1
 
         val = mul * int(val[1:])
+        self.val = val
 
-        ins_set.append([ins, val])
+    def __str__(self):
+        return f"{self.opcode} {self.val}"
 
-def switch_ins(x):
-    print(x)
-    if ins_set[x][0] == "jmp":
-        ins_set[x][0] = "nop"
+class Machine:
+    FINISHED = 0
+    LOOP = 1
+    OUT_OF_BOUNDS = 2
+
+    def __init__(self, input):
+        self.instructions = []
+        self.visited = set()
+        self.acc_val = 0
+        self.pos = 0
+
+        for line in input:
+            self.instructions.append(Instruction(line))
+
+    def run(self):
+        self.reset()
+        while True:
+            if self.pos == len(self.instructions):
+                return Machine.FINISHED
+
+            if self.pos > len(self.instructions):
+                return Machine.OUT_OF_BOUNDS
+
+            if self.pos in self.visited:
+                return Machine.LOOP
+
+            ins = self.instructions[self.pos]
+            
+            self.visited.add(self.pos)
+
+            if ins.opcode == Instruction.ACC:
+                self.acc_val += ins.val
+                self.pos += 1
+            elif ins.opcode == Instruction.JMP:
+                self.pos += ins.val
+            else:
+                self.pos += 1
+
+    def switch_ins(self, switch_pos, ins1, ins2):
+        switched = False
+        if self.instructions[switch_pos].opcode == ins1:
+            self.instructions[switch_pos].opcode = ins2
+            switched = True
+        elif self.instructions[switch_pos].opcode == ins2:
+            self.instructions[switch_pos].opcode = ins1
+            switched = True
+        
+        return switched
+
+    def reset(self):
+        self.visited = set()
+        self.acc_val = 0
+        self.pos = 0
+
+machine = Machine(input)
+machine.run()
+# part1
+print(machine.acc_val)
+
+for switch_pos in range(len(input)):
+    # print(switch_pos)
+    if not machine.switch_ins(switch_pos, Instruction.JMP, Instruction.NOP):
+        # no need to run if no switch was made
+        continue
+
+    if machine.run() == Machine.FINISHED:
+        print(machine.acc_val)
+        break
     else:
-        ins_set[x][0] = "jmp"
-
-convert()
-while True:
-
-    if changed_ins >= 0:
-        switch_ins(changed_ins)
-
-    changed_ins += 1
-    while (changed_ins < len(ins_set)):
-        if ins_set[changed_ins][0]         == "jmp" or ins_set[changed_ins][0] == "nop":
-            switch_ins(changed_ins)
-            break
-        changed_ins +=1
-
-    pos = 0
-    acc = 0
-    visited = set()
-
-    print(ins_set)
-    while True:
-        if pos == len(ins_set):
-            print(acc)
-            exit(0)
-
-        if pos > len(ins_set):
-            print("too far")
-            break
-
-        if pos in visited:
-            print("loop")
-            break
-
-        ins_t = ins_set[pos]
-        ins = ins_t[0]
-        val = ins_t[1]
-        visited.add(pos)
-
-        # print(f"{ins}, {val}")
-        if ins == "acc":
-            acc += val
-            pos += 1
-        elif ins == "jmp":
-            pos += val
-        else:
-            pos += 1
+        # undo the switch
+        machine.switch_ins(switch_pos, Instruction.JMP, Instruction.NOP)
