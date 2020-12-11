fd0cb6a6e7391bf145bda8333a995d2dcbc2f428
Omer Karaduman <omer.karaduman@tomtom.com>
2020-12-11 11:09:32 +0100

    day11 cleanup

--- 2020/day11/code.py
+++ 2020/day11/code.py
@@ -1,4 +1,4 @@
-import os, sys, copy
+import os, sys, itertools
 
 grid = {}
 grid2 = {}
@@ -13,196 +13,88 @@ for i in range(imax):
 
     jmax = len(line)
     for j in range(jmax):
-        grid[(i,j)] = line[j]
-        grid2[(i,j)] = line[j]
-
-
-
-
+        grid2[(i,j)] = grid[(i,j)] = line[j]
 
 def is_occ(i, j):
     cell =  grid.get((i, j))
-    #print(cell)
     if cell and cell == "#":
         return 1
     else:
         return 0
+
+def is_occ_towards(i, j, di, dj):
+    ix = i
+    jx = j
+    cell = grid.get((ix, jx))
+    while cell:
+        ix += di
+        jx += dj
+        cell = grid.get((ix,jx))
+
+        if is_occ(ix, jx):
+            return 1
+
+        if cell and grid.get((ix, jx)) == "L":
+            break
+    
+    return 0
 
 def log(gr):
     for i in range(imax):
         str = ""
         for j in range(jmax):
             str += gr[(i,j)]
-
         print(str)
-
     print("\n\n")
 
-    
-def occ_count(i, j):
+dir_perms = [x for x in itertools.product([-1, 0, 1], [-1, 0, 1]) if x[0] or x[1]]
+
+def occ_count_part2(i, j):
     occ = 0
-    ix = i
-    jx = j
-    cell = grid.get((ix, jx))
-    while cell:
-        ix -= 1
-        cell = grid.get((ix,jx))
 
-        if is_occ(ix, jx):
-            occ += 1
-            break
-
-        if cell and grid.get((ix, jx)) == "L":
-            break
-
-    ix = i
-    jx = j
-    cell = grid.get((ix, jx))
-    while cell:
-        ix += 1
-        cell = grid.get((ix,jx))
-
-        if cell and is_occ(ix, jx):
-            occ += 1
-            break
-
-        if cell and grid.get((ix, jx)) == "L":
-            break
-
-    ix = i
-    jx = j
-    cell = grid.get((ix, jx))
-    while cell:
-        jx -= 1
-        cell = grid.get((ix,jx))
-
-        if cell and is_occ(ix, jx):
-            occ += 1
-            break
-
-        if cell and grid.get((ix, jx)) == "L":
-            break
-
-    ix = i
-    jx = j
-    cell = grid.get((ix, jx))
-    while cell:
-        jx += 1
-        cell = grid.get((ix,jx))
-
-        if cell and is_occ(ix, jx):
-            occ += 1
-            break
-
-        if cell and grid.get((ix, jx)) == "L":
-            break
-
-
-
-    ix = i
-    jx = j
-    cell = grid.get((ix, jx))
-    while cell:
-        ix -= 1
-        jx -= 1
-        cell = grid.get((ix,jx))
-
-        if cell and is_occ(ix, jx):
-            occ += 1
-            break
-
-        if cell and grid.get((ix, jx)) == "L":
-            break
-
-    ix = i
-    jx = j
-    cell = grid.get((ix, jx))
-    while cell:
-        ix -= 1
-        jx += 1
-        cell = grid.get((ix,jx))
-
-        if cell and is_occ(ix, jx):
-            occ += 1
-            break
-
-        if cell and grid.get((ix, jx)) == "L":
-            break
-
-    ix = i
-    jx = j
-    cell = grid.get((ix, jx))
-    while cell:
-        ix += 1
-        jx += 1
-        cell = grid.get((ix,jx))
-
-        if cell and is_occ(ix, jx):
-            occ += 1
-            break
-
-
-        if cell and grid.get((ix, jx)) == "L":
-            break
-
-    ix = i
-    jx = j
-    cell = grid.get((ix, jx))
-    while cell:
-        ix += 1
-        jx -= 1
-        cell = grid.get((ix,jx))
-
-        if cell and is_occ(ix, jx):
-            occ += 1
-            break
-
-        if cell and grid.get((ix, jx)) == "L":
-            break
-
-
+    for p in dir_perms:
+        occ += is_occ_towards(i, j, p[0], p[1])
 
     return occ
 
-def occ_count_(i, j):
-    return is_occ(i-1, j-1) + is_occ(i-1, j) + is_occ(i-1, j+1) + \
-           is_occ(i, j-1) + is_occ(i, j+1) +\
-           is_occ(i+ 1, j-1) + is_occ(i+ 1, j)  + is_occ(i+ 1, j+1) 
-
-def move():
+def occ_count_part1(i, j):
+    perms = [x for x in itertools.product([i - 1, i, i + 1], [j - 1, j, j + 1])]
+    perms.remove((i, j))
+    return sum(list(map(lambda x: is_occ(x[0], x[1]), perms)))
+    
+def move(max_neighbors, check_func):
     for k, v in grid2.items():
         grid[k] = v
 
-    log(grid)
+    # log(grid)
     changed = False
     for i in range(imax):
         for j in range(jmax):
             cur = grid[(i, j)]
-            occ = occ_count(i, j)
-            #print(occ)
+            occ = check_func(i, j)
+
             if cur == "L" and occ == 0:
-                grid2[(i,j)] = "#"
+                grid2[(i, j)] = "#"
                 changed = True
-            elif cur == "#" and occ >= 5:
+            elif cur == "#" and occ >= max_neighbors:
                 grid2[(i, j)] = "L"
                 changed = True
             else:
-                grid2[(i,j)] = grid[(i,j)]
+                grid2[(i, j)] = grid[(i, j)]
 
     return changed
 
+def solve(max_neighbors, check_func):
+    while move(max_neighbors, check_func):
+        continue
 
-x = 0
-while move():
-    #print("rolling")
-    continue
+    cnt = 0
+    for i in range(imax):
+        for j in range(jmax):
+            if grid[(i, j)] == "#":
+                cnt +=1
 
+    print(cnt)
 
-cnt = 0
-for i in range(imax):
-    for j in range(jmax):
-        if grid[(i, j)] == "#":
-            cnt +=1
-
-
-print(cnt)
+solve(4, occ_count_part1)
+solve(5, occ_count_part2)
