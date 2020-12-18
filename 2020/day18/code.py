2fcb8a8b3a234bee15f10c811cfa79985cc4f995
Omer Karaduman <omer.karaduman@tomtom.com>
2020-12-18 06:35:34 +0100

    day18, p2, rush

--- 2020/day18/code.py
+++ 2020/day18/code.py
@@ -5,7 +5,8 @@ with open(os.path.join(sys.path[0], 'input'), 'r') as in_file:
     input = in_file.readlines()
 
 
-def calcops(oplist):
+def calcops_p1(oplist):
+    """no precedence, calculate as you go"""
     res =0 
     lastop = "+"
     for op in oplist:
@@ -21,11 +22,32 @@ def calcops(oplist):
 
     return res
 
+def getsum(mstr):
+    ops = mstr.split('+')
+
+    res = 0
+    for op in ops:
+        res += int(op)
+
+    return res
 
 
-def calculate(str):
+def calcops_p2(oplist):
+    """split by multiplications, find sum in each section, multiply results"""
+    res = 1
+    opstr = "".join(list(map(lambda x:str(x),oplist)))
+    
+    mults = opstr.split('*')
+
+    for m in mults:
+        res *= getsum(m)
+
+    return res
+
+
+def calculate(str, funcref):
+    """split terms by paranthesis groups and manage overall calculation"""
     ops = []
-    lastnum = 0
 
     for ch in str:
         if ch in "0123456789":
@@ -39,18 +61,19 @@ def calculate(str):
                 subops.insert(0, prev)
                 prev= ops.pop()
 
-            res = calcops(subops)
+            res = funcref(subops)
             ops.append(res)
 
-    return calcops(ops)
+    return funcref(ops)
 
 
 p1 = 0
+p2 = 0
 for line in input:
     line = line.strip()
-    res = calculate(line)
-    print(res)
-    p1 += res
+    p1 += calculate(line, calcops_p1)
+    p2 += calculate(line, calcops_p2)
 
 print(p1)
+print(p2)
 
