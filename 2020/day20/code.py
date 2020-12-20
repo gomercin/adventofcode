4f5c36b5e7be2286426cd422f429d51a5de4c5f4
Omer Karaduman <omer.karaduman@tomtom.com>
2020-12-20 11:42:32 +0100

    day20, both parts, not proud :)

--- 2020/day20/code.py
+++ 2020/day20/code.py
@@ -1,5 +1,7 @@
 import os, sys
 from itertools import permutations
+from copy import deepcopy
+import numpy as np
 
 TOP, LEFT, BOTTOM, RIGHT = 0, 1, 2, 3
 
@@ -19,6 +21,15 @@ class Tile:
         self.bottom_connects = {}
         self.right_connects = {}
         self.left_connects = {}
+
+        tmpimage = [[] for _ in range(len(lines[1]) -2)]
+        self.images = []
+
+        for i in range(1, len(self.lines) - 2):
+            tmpimage[i-1] = (list(self.lines[i + 1]))[1:-1]
+
+        self.image = np.array(tmpimage)
+        if self.id == 2311 or self.id == 3571: print(self.image)
 
         self.parse()
         
@@ -44,21 +55,31 @@ class Tile:
                 right |= 0x1
 
         rotations = [(top, left, bottom, right)]
+        self.images.append(self.image)
         tmp_top, tmp_left, tmp_bottom, tmp_right = top, left, bottom, right
+        tmpimg = self.image
         for _ in range(3):
             new_var = (tmp_right, self.reverse(tmp_top), tmp_left, self.reverse(tmp_bottom))
             rotations.append(new_var)
             tmp_top, tmp_left, tmp_bottom, tmp_right = new_var
 
+            tmpimg = np.rot90(tmpimg)
+            self.images.append(tmpimg)
+
         self.edge_variations.extend(rotations)
 
-        for rot in rotations:
+        for i, rot in enumerate(rotations):
             tmp_top, tmp_left, tmp_bottom, tmp_right = rot[0], rot[1], rot[2], rot[3]
             # flip rotation horizontal
             self.edge_variations.append((tmp_bottom, self.reverse(tmp_left), tmp_top, self.reverse(tmp_right)))
+            
+            self.images.append(np.flip(self.images[i], 0))
 
             # flip rotation vertical:
             self.edge_variations.append((self.reverse(tmp_bottom), tmp_left, self.reverse(tmp_top), tmp_right))
+            self.images.append(np.flip(self.images[i], 1))
+
+        assert len(self.images) == len(self.edge_variations)
 
 
     def reverse(self, num):
@@ -301,6 +322,93 @@ grid = {}
 index = (0, 0)
 
 correct_grid = None
+monster = ["                  # ", 
+           "#    ##    ##    ###", 
+           " #  #  #  #  #  #   "]
+
+monster_img = np.array(
+    [list(monster[0]), list(monster[1]), list(monster[2])]
+)
+
+print(monster_img)
+
+def grid_to_image(grid):
+    img = [[] for _ in range(SIZE * 8)]
+
+    for i in range(SIZE):
+        for j in range(SIZE):
+            tile_id, variant = grid[(i, j)]
+            tile = all_tiles[tile_id]
+            tile_img = tile.images[variant]
+            
+            for img_r in range(len(tile_img)):
+                for pixel in tile_img[img_r]:
+                    img[i * 8 + img_r].append(pixel)
+
+    assert len(img) == len(img[0]) == SIZE * 8
+
+    return img
+
+def process_monsters(img):
+    found = False
+    tmpimg = deepcopy(img)
+    for row in range(len(img) - 3):
+        for col in range(len(img[0]) - len(monster_img[0])):
+            skip = False
+            tmptmpimg = deepcopy(tmpimg)
+            for mrow, monsterrow in enumerate(monster_img):
+                if skip: break
+                for mcol, char in enumerate(monsterrow):
+                    if char == "#":
+                        if tmptmpimg[row + mrow][col + mcol] != "#":
+                            skip = True
+                            break
+                        else:
+                            tmptmpimg[row + mrow][col + mcol] = "O"
+
+            if skip == False:
+                found = True
+                tmpimg = deepcopy(tmptmpimg)
+
+    return found, tmpimg
+
+
+
+def solve_monsters(grid):
+    all_pictures = []
+    big_picture = grid_to_image(grid)
+    
+    all_pictures.append(big_picture)
+    tmp_picture = big_picture
+    for _ in range(3):
+        tmp_picture = np.rot90(tmp_picture)
+        all_pictures.append(tmp_picture)
+
+    flips = []
+
+    for pic in all_pictures:
+        flips.append(np.flip(pic, 0))
+        flips.append(np.flip(pic, 1))
+
+    all_pictures.extend(flips)
+
+    for pic in all_pictures:
+        res, newimg = process_monsters(pic)
+
+        if res:
+            print("found something!")
+            print(newimg)
+
+            cnt = 0
+            for row in newimg:
+                for ch in row:
+                    if ch == "#":
+                        cnt += 1
+
+            #p2
+            print(cnt)
+
+
 
 for tile_id in tile_ids:
     unused_tiles = set(tile_ids)
@@ -320,4 +428,6 @@ for tile_id in tile_ids:
             id4 = all_tiles[c4].id
             # p1
             print(id1, id2, id3, id4 , id1 * id2 * id3 * id4)
+
+            solve_monsters(res)
             exit(0)
