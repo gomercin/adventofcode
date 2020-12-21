251cec2c380e60d1610077620dc133e7f05fdd49
Omer Karaduman <omer.karaduman@tomtom.com>
2020-12-21 06:56:02 +0100

    day21, small cleanup

--- 2020/day21/code.py
+++ 2020/day21/code.py
@@ -3,7 +3,6 @@ from itertools import permutations
 from copy import deepcopy
 import numpy as np
 
-
 input = []
 alltext = ""
 with open(os.path.join(sys.path[0], 'input'), 'r') as in_file:
@@ -11,11 +10,9 @@ with open(os.path.join(sys.path[0], 'input'), 'r') as in_file:
     in_file.seek(0)
     alltext = in_file.read()
 
-
 allergen_possibilities = {} # allergen name, to list of sets
 ingredient_counts = {} 
 foods = [] # list of Food objects
-
 
 class Food:
     def __init__(self, line):
@@ -44,48 +41,48 @@ class Food:
         print("   ingredients: " + str(self.ingredients))
         print("   allergens  : " + str(self.allergens))
 
-
-
 for line in input:
     line = line.strip()
     food = Food(line)
-    food.print()
+    # food.print()
     foods.append(food)
 
 allergen_possibilities_reduced = {}
+# for each allergen, intersect the ingredients it can possibly be found
+# this gives a reduced list of potential ingredients
 for allergen, possibilities in allergen_possibilities.items():
     intersection = set.intersection(*possibilities)
     allergen_possibilities_reduced[allergen] = intersection
 
 allergen_possibilities_certain = {}
+allergics = set()
 
+# loop "a few" times, normally we should loop until each allergen is mapped to a single ingredient
+# but looping 10 times was good and fast enough, and I am too lazy to write the other check :)
 for i in range(10):
     for allergen, possibilities in allergen_possibilities_reduced.items():
         if len(possibilities) == 1:
+            # this allergen is certainly in this only ingredient
+            # so this ingredient can be removed from other allergens
             assert allergen not in allergen_possibilities_certain
-            allergen_possibilities_certain[allergen] = list(possibilities)[0]
+            ingredient = list(possibilities)[0]
+            allergen_possibilities_certain[allergen] = ingredient
+            allergics.add(ingredient)
             a = list(possibilities)[0]
             for a2, p2 in allergen_possibilities_reduced.items():
                 if a in p2:
                     allergen_possibilities_reduced[a2].remove(a)
 
-print(allergen_possibilities_certain)
-
 cnt = 0
 
-for v in allergen_possibilities_certain.values():
-    del ingredient_counts[v]
-
-
-
 for k, v in ingredient_counts.items():
-    cnt += v
+    if k not in allergics:
+        cnt += v
 
+# p1
 print(cnt)
 
-
 allergens = sorted(list(allergen_possibilities_certain.keys()))
-
 
 res = ""
 
@@ -93,4 +90,5 @@ for allergen in allergens:
     res += ","
     res += allergen_possibilities_certain[allergen]
 
-print(res)
+# p2
+print(res[1:])
